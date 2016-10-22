030c83dea4009bf25cffb211bf9c355cacc5ef01
Yuankun Zhang <fmcocoa@gmail.com>
2016-10-22 20:13:24 +0800

    PrimaryType

---
+++ SwiftJsonSchema/PrimaryType.swift
@@ -0,0 +1,19 @@
+//
+//  PrimaryType.swift
+//  SwiftJsonSchema
+//
+//  Created by Yuankun Zhang on 22/10/2016.
+//  Copyright © 2016 Yuankun Zhang. All rights reserved.
+//
+
+import Foundation
+
+enum PrimaryType : String {
+    case array      = "array"
+    case boolean    = "boolean"
+    case integer    = "integer"
+    case number     = "number"
+    case null       = "null"
+    case object     = "object"
+    case string     = "string"
+}
---
+++ SwiftJsonSchema/SwiftJsonSchema.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftJsonSchema.swift
+//  SwiftJsonSchema
+//
+//  Created by Yuankun Zhang on 22/10/2016.
+//  Copyright © 2016 Yuankun Zhang. All rights reserved.
+//
+
+import Foundation
