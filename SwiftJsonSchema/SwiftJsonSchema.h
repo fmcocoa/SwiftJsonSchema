d4df6997a53276562fcfd70d363eca0f37d90600
Yuankun Zhang <fmcocoa@gmail.com>
2016-10-22 19:02:05 +0800

    Initial Commit

---
+++ SwiftJsonSchema/SwiftJsonSchema.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJsonSchema.h
+//  SwiftJsonSchema
+//
+//  Created by Yuankun Zhang on 22/10/2016.
+//  Copyright © 2016 Yuankun Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJsonSchema.
+FOUNDATION_EXPORT double SwiftJsonSchemaVersionNumber;
+
+//! Project version string for SwiftJsonSchema.
+FOUNDATION_EXPORT const unsigned char SwiftJsonSchemaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJsonSchema/PublicHeader.h>
+
+
